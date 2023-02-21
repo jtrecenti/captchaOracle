9186be77cb52229342462728a27b25e25f78fb4c
Julio Trecenti <julio.trecenti@gmail.com>
2023-02-21 16:38:50 -0300

    add stringr dependency

--- R/utils-global.R
+++ R/utils-global.R
@@ -1,1 +1,8 @@
 utils::globalVariables(c("super", ".."))
+
+fake_stringr <- function() {
+  # this forces a dependency on stringr package, which is used in the
+  # dataset function. We need to do this because R CMD CHECK can not
+  # detect dependencies inside torch modules.
+  stringr::str_detect
+}
