32044baf435107f4405fba2d59d931da40b0d4a3
Julio Trecenti <julio.trecenti@gmail.com>
2023-02-21 16:37:32 -0300

    write accuracy documentation

--- R/metrics.R
+++ R/metrics.R
@@ -1,5 +1,22 @@
 #' Captcha accuracy metric with oracle
 #'
+#' This object is used to calculate the accuracy of the model in the
+#' fitting process.
+#'
+#' This function is a generator created using [luz::luz_metric()] function.
+#' It has a `initialize()` method that sets the total number of instances
+#' and total number of correct predictions as zero. For any minibatch, it
+#' has an `update()` method that updates the total number of instances and
+#' total number of correct predictions with new data. Finally, it has a
+#' `compute()` method that calculates accuracy from the total number of
+#' correct predictions and total number of instances.
+#'
+#' Training data has a different structure than validation
+#' data: training data includes both complete and incomplete labels
+#' from the oracle, but validation includes only complete labels. Accuracy
+#' is calculated using only when we have at least one complete label in the
+#' minibatch. Accuracy is calculated considering only the complete labels.
+#'
 #' @importFrom luz luz_metric
 #' @export
 captcha_accuracy_oracle <- luz::luz_metric(
@@ -21,7 +38,7 @@ captcha_accuracy_oracle <- luz::luz_metric(
     if (length(ind_ok) > 0) {
       pred <- torch::torch_argmax(preds[ind_ok,..,drop=FALSE], dim = 3)
 
-      # bases de treino e teste apresentam estruturas diferentes
+      # train and validation have different structures
       if (is.list(target$y)) {
         tgt <- torch::torch_argmax(torch::torch_stack(target$y[ind_ok])$squeeze(2L), dim = 3)
       } else {
